5a3a8b5525da7a7ffebf038382fe2452b4e7671c
Nam Mai <mdnam2410@gmail.com>
2021-10-05 22:19:58 +0700

    Fix ambiguous macro names
    The macro names collide with classes ConsoleInput and ConsoleOutput, defined
    in `machine/console.h`. The change resonates to test/shell.c

--- nachos/NachOS-4.0/code/userprog/syscall.h
+++ nachos/NachOS-4.0/code/userprog/syscall.h
@@ -127,8 +127,8 @@ typedef int OpenFileId;
  * the console device.
  */
 
-#define ConsoleInput 0
-#define ConsoleOutput 1
+#define CONSOLEINPUT 0
+#define CONSOLEOUTPUT 1
 
 /* Create a Nachos file, with name "name" */
 /* Note: Create does not open the file.   */
