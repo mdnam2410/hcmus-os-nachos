0a03a59a5caabbe5552271aa068ceb54ee50d5c2
maxminlevel <vvtoan35@gmail.com>
2021-10-06 09:31:18 +0100

    done 2/3 again

--- nachos/NachOS-4.0/code/userprog/exception.cc
+++ nachos/NachOS-4.0/code/userprog/exception.cc
@@ -164,142 +164,15 @@ void ExceptionHandler(ExceptionType which)
 			kernel->interrupt->Halt();
 			return;
 		case SC_ReadNum:
-			// Input: K co
-			// Output: Tra ve so nguyen doc duoc tu man hinh console.
-			// Chuc nang: Doc so nguyen tu man hinh console.
-			char *buffer;
-			int MAX_BUFFER = 255;
-			buffer = new char[MAX_BUFFER + 1];
-			int numbytes = kernel->synchConsoleIn->GetChar(); //->Read(buffer, MAX_BUFFER); // doc buffer toi da MAX_BUFFER ki tu, tra ve so ki tu doc dc
-			int number = 0;											// so luu ket qua tra ve cuoi cung
-
-			/* Qua trinh chuyen doi tu buffer sang so nguyen int */
-
-			// Xac dinh so am hay so duong
-			bool isNegative = false; // Gia thiet la so duong.
-			int firstNumIndex = 0;
-			int lastNumIndex = 0;
-			if (buffer[0] == '-')
-			{
-				isNegative = true;
-				firstNumIndex = 1;
-				lastNumIndex = 1;
-			}
-
-			// Kiem tra tinh hop le cua so nguyen buffer
-			for (int i = firstNumIndex; i < numbytes; i++)
-			{
-				if (buffer[i] == '.') /// 125.0000000 van la so
-				{
-					int j = i + 1;
-					for (; j < numbytes; j++)
-					{
-						// So khong hop le
-						if (buffer[j] != '0')
-						{
-							printf("\n\n The integer number is not valid");
-							DEBUG('a', "\n The integer number is not valid");
-							kernel->machine->WriteRegister(2, 0);
-							IncreasePC();
-							delete buffer;
-							return;
-						}
-					}
-					// la so thoa cap nhat lastNumIndex
-					lastNumIndex = i - 1;
-					break;
-				}
-				else if (buffer[i] < '0' && buffer[i] > '9')
-				{
-					printf("\n\n The integer number is not valid");
-					DEBUG('a', "\n The integer number is not valid");
-					kernel->machine->WriteRegister(2, 0);
-					IncreasePC();
-					delete buffer;
-					return;
-				}
-				lastNumIndex = i;
-			}
-
-			// La so nguyen hop le, tien hanh chuyen chuoi ve so nguyen
-			for (int i = firstNumIndex; i <= lastNumIndex; i++)
-			{
-				number = number * 10 + (int)(buffer[i] - 48);
-			}
-
-			// neu la so am thi * -1;
-			if (isNegative)
-			{
-				number = number * -1;
-			}
-			kernel->machine->WriteRegister(2, number);
-			IncreasePC();
-			delete buffer;
-			return;
-
+			break;
 		case SC_PrintNum:
-			// Input: mot so integer
-			// Output: khong co
-			// Chuc nang: In so nguyen len man hinh console
-			int number = kernel->machine->ReadRegister(4);
-			if (number == 0)
-			{
-				gSynchConsole->Write("0", 1); // In ra man hinh so 0
-				IncreasePC();
-				return;
-			}
-
-			/*Qua trinh chuyen so thanh chuoi de in ra man hinh*/
-			bool isNegative = false; // gia su la so duong
-			int numberOfNum = 0;	 // Bien de luu so chu so cua number
-			int firstNumIndex = 0;
-
-			if (number < 0)
-			{
-				isNegative = true;
-				number = number * -1; // Nham chuyen so am thanh so duong de tinh so chu so
-				firstNumIndex = 1;
-			}
-
-			int t_number = number; // bien tam cho number
-			while (t_number)
-			{
-				numberOfNum++;
-				t_number /= 10;
-			}
-
-			// Tao buffer chuoi de in ra man hinh
-			char *buffer;
-			int MAX_BUFFER = 255;
-			buffer = new char[MAX_BUFFER + 1];
-			for (int i = firstNumIndex + numberOfNum - 1; i >= firstNumIndex; i--)
-			{
-				buffer[i] = (char)((number % 10) + 48);
-				number /= 10;
-			}
-			if (isNegative)
-			{
-				buffer[0] = '-';
-				buffer[numberOfNum + 1] = 0;
-				gSynchConsole->Write(buffer, numberOfNum + 1);
-				delete buffer;
-				IncreasePC();
-				return;
-			}
-			buffer[numberOfNum] = 0;
-			gSynchConsole->Write(buffer, numberOfNum);
-			delete buffer;
-			IncreasePC();
-			return;
-
+			break;
 		case SC_ReadChar:
 			//Input: Khong co
 			//Output: Duy nhat 1 ky tu (char)
 			//Cong dung: Doc mot ky tu tu nguoi dung nhap
 			char c = kernel->synchConsoleIn->GetChar();
 			kernel->machine->WriteRegister(2, c);
-			//IncreasePC(); // error system
-			//return;
 			break;
 
 		case SC_PrintChar:
@@ -307,43 +180,12 @@ void ExceptionHandler(ExceptionType which)
 			// Output: Ki tu(char)
 			// Cong dung: Xuat mot ki tu la tham so arg ra man hinh
 			char c = (char)kernel->machine->ReadRegister(4); // Doc ki tu tu thanh ghi r4
-			gSynchConsole->Write(&c, 1);					 // In ky tu tu bien c, 1 byte
+			kernel->synchConsoleOut->PutChar(c);					 // In ky tu tu bien c, 1 byte
 			//IncreasePC();
 			break;
 
 		case SC_ReadString:
-			// Input: Buffer(char*), do dai toi da cua chuoi nhap vao(int)
-			// Output: Khong co
-			// Cong dung: Doc vao mot chuoi voi tham so la buffer va do dai toi da
-			int virtAddr, length;
-			char *buffer;
-			virtAddr = kernel->machine->ReadRegister(4); // Lay dia chi tham so buffer truyen vao tu thanh ghi so 4
-			length = kernel->machine->ReadRegister(5);	 // Lay do dai toi da cua chuoi nhap vao tu thanh ghi so 5
-			buffer = User2System(virtAddr, length);		 // Copy chuoi tu vung nho User Space sang System Space
-			gSynchConsole->Read(buffer, length);		 // Goi ham Read cua SynchConsole de doc chuoi
-			System2User(virtAddr, length, buffer);		 // Copy chuoi tu vung nho System Space sang vung nho User Space
-			delete buffer;
-			IncreasePC(); // Tang Program Counter
-			return;
-			//break;
-
 		case SC_PrintString:
-			// Input: Buffer(char*)
-			// Output: Chuoi doc duoc tu buffer(char*)
-			// Cong dung: Xuat mot chuoi la tham so buffer truyen vao ra man hinh
-			int virtAddr;
-			char *buffer;
-			virtAddr = kernel->machine->ReadRegister(4); // Lay dia chi cua tham so buffer tu thanh ghi so 4
-			buffer = User2System(virtAddr, 255);		 // Copy chuoi tu vung nho User Space sang System Space voi bo dem buffer dai 255 ki tu
-			int length = 0;
-			while (buffer[length] != 0)
-				length++;							  // Dem do dai that cua chuoi
-			gSynchConsole->Write(buffer, length + 1); // Goi ham Write cua SynchConsole de in chuoi
-			delete buffer;
-			//IncreasePC(); // Tang Program Counter
-			//return;
-			break;
-
 		default:
 			break;
 		}
