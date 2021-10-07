a13acacb38d4aab9ce1ea1759b6cbd70a34b299f
Nam Mai <mdnam2410@gmail.com>
2021-10-07 11:20:46 +0700

    Add help program

--- nachos/NachOS-4.0/code/test/help.c
+++ nachos/NachOS-4.0/code/test/help.c
@@ -2,6 +2,18 @@
 
 int main()
 {
+    char* s[] = {
+        "Group members\n",
+        "ID       | Name\n",
+        "-------- | ----------------\n", 
+        "19120298 | Mai Duy Nam\n",
+        "19120690 | Vo Van Toan\n",
+        "19120491 | Dang Thai Duy\n"
+    };
 
+    int i;
+    for (i = 0; i < 6; ++i)
+        PrintString(s[i]);
+    Halt();
     return 0;
 }
